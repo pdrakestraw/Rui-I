8a7fe24ab4f78cdf2ce633496bc0296bdb730fc3
Paul Rakestraw <prakestr@uncc.edu>
2025-09-10 20:01:33 -0400

    ReadMe Update and Comments made

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Rui-I
-Chatbot that has text to speech and responds in real time. 
+# RUI-I
+
+Chatbot assistant that responds in real time with text to speach answers
+
+## Installation
+
+**Clone the repo**
+
+git clone https://github.com/username/project.git
+cd project 
+
+**Install dependencies**
+pip install -r requirements.txt
+
+## Usage
+
+Currently the program only runs when using the terminal 
+  python3 main.py
+but hopefully in future versions it will run on voice activation. 
+
+## Version History
+
+**In Development**
+
+## Future Work
+
+- Adding voice recogintion
+- Raspberry Pi integration 
+- Better voices and responses
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 
 p = pyaudio.PyAudio()
 
+#Loading and configuring Piper voice, 
 voice = PiperVoice.load("voices/en_GB-northern_english_male-medium.onnx")
 syn_config = SynthesisConfig(
     volume=0.5,
@@ -15,12 +16,11 @@ syn_config = SynthesisConfig(
     normalize_audio=False, 
 )
 
-
-
+# Loading the API Key, use your own API key here. See ReadMe for instructions
 load_dotenv()
 assistant = OpenAI(api_key=os.getenv("MY_API_KEY"))
 
-
+# Initializing pyaudio, this is what plays the audio from computer without saving to file
 stream = p.open(
     format=pyaudio.paInt16,
     channels=1,
@@ -28,6 +28,7 @@ stream = p.open(
     output=True
 )
 
+# Start of the loop
 text = ""
 while(text != "exit"):
     text = input("Text: ")
@@ -43,7 +44,11 @@ while(text != "exit"):
         n=1
     )
     message = response.choices[0].message.content.strip()
+
+    # In the terminal, it'll display the amount of tokens used for each response
     print("Tokens Used: ", response.usage.total_tokens)
+
+    # 
     for chunk in voice.synthesize(message, syn_config):
         if chunk.audio_int16_bytes:
             stream.write(chunk.audio_int16_bytes)
